a163cea35bf127f18d9f9d4c013f712c9475d0fa
JoseFuentesR <caca.candente@gmail.com>
2020-10-13 19:47:13 -0300

    tratando de hacer ejercicio mario

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import ejercicio2 from './modules/dos.js';
 var caja = document.getElementById("caja");
 
 window.cargarTabla = ejercicio1.cargar;
-window.mario = ejercicio2.estado;
+window.estado = ejercicio2.piedra;
 document.getElementById("uno").addEventListener("click",() => {
     caja.innerHTML = ejercicio1.data
     ejercicio1.cargar();
@@ -11,4 +11,5 @@ document.getElementById("uno").addEventListener("click",() => {
 });
 document.getElementById("dos").addEventListener("click",() => {
 caja.innerHTML = ejercicio2.data
+ejercicio2.piedra();
 });
--- js/modules/dos.js
+++ js/modules/dos.js
@@ -4,14 +4,20 @@ export default{
 
     data:`
    <h1>Autor: Jose Fuentes </h1>
-   <h3 id="txt1"><h3>
-   <button id="piedra">Piedra</button><br>
-    <button id="papel">Papel</button><br>
-    <button id="tijera">Tijera</button><br>
+   <h3 id="txt1">Estado :normal</h3>
+   <h3 id=txt2>vidas:3</h3> 
+   <button id="flor">Flor</button><br>
+    <button id="papel">Pluma</button><br>
+    <button onclick="piedra()">Pierde</button><br>
     <img src="img/mario1.png" alt="" id="imagen"/>
     
     `,
-    estado:function(){
-
-    }
+    piedra:function(){
+        var imagen = document.getElementById("imagen")
+       var estado = document.getElementById("txt1");
+       var vidas = document.getElementById("txt2");
+       estado = "estado:volador"
+       vidas = "vidas:3"
+       imagen.innerHtml='<img src="img/mario2.jpg">' 
+    },
 };
